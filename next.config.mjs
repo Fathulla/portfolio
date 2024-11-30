cfb2f613fed73c8036a7a4e110aaea6b9eedabdc
Fathulla Rashidov <fathullarasidov239@gmail.com>
2024-11-30 23:20:47 +0500

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import { withSentryConfig } from "@sentry/nextjs";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
